5b9655a33a808c983e01e046f0c04281437b4a5d
Hugomelo <salles.hugo@gmail.com>
2022-07-25 16:48:56 -0400

    added post request and dynamic get route

--- index.js
+++ index.js
@@ -3,11 +3,45 @@ import express from "express";
 
 // initialize express server
 const app = express();
+app.use(express.json());
+
+
+// add a new student (in theory)
+app.post('/students', (request, response) => {
+    const newStudent = request.body;
+    students.push(newStudent);
+    response.status(201).send(students);
+});
+
 
 // list requests that we want express to respond to
 app.get('/hello', (request, response) => {
     //do something...
+    console.log('xxx Hello World! xxx');
     response.send('Hello World!');
+})
+
+const students = [
+    { firstName: 'Jonathan', lastName: 'Vegas' },
+    { firstName: 'Brigette', lastName: 'Lemus' },
+    { firstName: 'Mason', lastName: 'Madaras' },
+    { firstName: 'Logan', lastName: 'McCalley' },
+ ];
+
+
+
+app.get('/students', (request, response) => {
+    
+     response.send(students); 
+})
+
+app.get('/students/:firstName', (request, response) => {
+    const student = students.find(stud => stud.firstName === request.params.firstName)
+    if (!student) {
+        response.status(404).send({ message: 'Student not found', success: false });
+        return;
+    }
+    response.send(student);
 })
 
 
